a430b007792be22a8169624d6dd8f41ff235e700
Albanianandproud <63110378+Albanianandproud@users.noreply.github.com>
2021-03-06 01:34:00 +0000

    Fixed Support Weapons & other
    -Added new GFX to the mod
    - Buffed Scavenging Decisions. No longer have a cooldown & give slightly more resources
    - Nerfed Red Lines' Industrialisation decisions. Slightly more PP for longer time.
    - Added Roadmap as a TXT file.
    - Buffed Generals max commander capacity. Should now be 10 instead of 4

--- Metro/common/defines/00_defines.lua
+++ Metro/common/defines/00_defines.lua
@@ -592,11 +592,11 @@ NMilitary = {
 	WAR_SCORE_AIR_WORTH_CAP = 1000,				-- the warscore limit we get from strategic bombing
 	WAR_SCORE_AIR_MONTHLY_FALLOFF = 10,			-- how much the warscore we got from the strategic bombing falls off every month.
 	
-	CORPS_COMMANDER_DIVISIONS_CAP = 20,			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
-	DIVISION_SIZE_FOR_XP = 8,                   -- how many battalions should a division have to count as a full divisions when calculating XP stuff
+	CORPS_COMMANDER_DIVISIONS_CAP = 16,			-- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
+	DIVISION_SIZE_FOR_XP = 5,                   -- how many battalions should a division have to count as a full divisions when calculating XP stuff
 	CORPS_COMMANDER_ARMIES_CAP = -1,			-- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
 	FIELD_MARSHAL_DIVISIONS_CAP = 24,			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
-	FIELD_MARSHAL_ARMIES_CAP = 4,				-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
+	FIELD_MARSHAL_ARMIES_CAP = 8,				-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
 
 	UNIT_LEADER_GENERATION_CAPITAL_CONTINENT_FACTOR = 100, --Integer factor to multiply manpower.
 
