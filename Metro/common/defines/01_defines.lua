0eddbf6186b44f0bbfdb6e70adb6b00c82a1ce60
Albanianandproud <Bigdickautomobiles@gmail.com>
2022-01-09 19:02:41 +0000

    ### **Update 09/01/2022** ###
    ### **Update 09/01/2022** ###
    - Added over 300 new provinces to the mod. More tunnels having more provinces making combat much better.
    - Added new flags into the mod. Thankyou to Kozwein#1618 for helping us with the new flags!
    - Fixed Reich's faction being called 'The'
    - Reduced Command Power daily gain to 0.25
    - Reduced Max Command Power to 100
    - Nerfed Community path of generic focus tree. Immigrants now only provides +1 weekly manpower instead of 2.
    - Free Ideals provides only 2 manpower instead of 4 and only provides 1 building slot instead of 2.
    - Infrastructure no longer provides resource boosts to state resource gain.
    ## Weapons ##
    - Increased the Cost of Duplets to 2 Scrap, 1 Alloy. And Ubonik to 3 Scrap, 1 Alloy
    - Reduced Defense of Duplets to 12. Soft attack to 11.
    - Reduced Defense of Ubonik to 14. Soft attack to 14.
    - Reduced Cost of Bastard to 22 IC, Reduced Cost of Heavy Bastard to 28 IC
    - Increased AP attack to Bastard to 4
    - Increased Hard Atack of Heavy Bastard to 5
    - Increased Hard Attack and AP of the Tihar to 13
    - Increased AP and Hard Attack of the Helsing to 18
    - Increased AP and Hard Attack of VSK-94 to 10
    - Increased Hard Attack and AP of Kalash 2012 to 14
    ## Vehicles ##
    - Hand Carts cost increased to 70 IC
    - Salvaged Carts increased to 85 IC
    - Reinforced Rail Carts increased to 105 IC
    - Breakthrough of Rail Carts increased by +2
    - Increaseed cost of Rail Cars by 15 IC each
    - Increased Resource cost of Rail Cars by +2 Scrap and Alloys
    - Increased Cost of all Rail Panzers by 25 IC
    - Scrap Tanks now cost +1 Scrap and Electronics
    - Prewar Carriages now cost +1 Scrap and Alloys
    - Prewar Carriages AP increased to 35
    - Panzers now cost 5 Srap, Alloys, 4 Components and 2 Electronics
    - Panzers soft attack reduced by 5 to 60
    - Panzers hard attack increased to 50 and AP to 50
    ## Localisation ##
    - Renamed Belorusskaya to the 'Free Farms'
    - Renamed Technicians to 'Kuznetsky Most'

--- Metro/common/defines/01_defines.lua
+++ Metro/common/defines/01_defines.lua
@@ -1,28 +1,33 @@
 ---Game Date---
 NDefines.NGame.START_DATE = "2028.1.1.12"
-NDefines.NGame.END_DATE = "2050.1.1.1"
+NDefines.NGame.END_DATE = "2055.1.1.1"
 
 
 ---Diplomacy Stuff---
-NDefines.NDiplomacy.TRUCE_BREAK_COST_PP = 150
-NDefines.NDiplomacy.NAP_EXPIRY_MONTHS = 26
-NDefines.NDiplomacy.NAP_UNBREAKABLE_MONTHS = 8
+NDefines.NDiplomacy.TRUCE_BREAK_COST_PP = 100
+NDefines.NDiplomacy.NAP_EXPIRY_MONTHS = 24
+NDefines.NDiplomacy.NAP_UNBREAKABLE_MONTHS = 6
 NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 5
 NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "2028.1.1.12"
-NDefines.NDiplomacy.GUARANTEE_COST = 150					
-NDefines.NDiplomacy.REVOKE_GUARANTEE_COST = 150
+NDefines.NDiplomacy.GUARANTEE_COST = 100			
+NDefines.NDiplomacy.REVOKE_GUARANTEE_COST = 100
 NDefines.NDiplomacy.BASE_CONDITIONAL_PEACE_WARESCORE_RATIO = 0.2
 NDefines.NDiplomacy.BASE_CONDITIONAL_PEACE_MONTHS = 3
 NDefines.NDiplomacy.PEACE_SCORE_PER_PASS = 0.35
 NDefines.NDiplomacy.PEACE_MIN_SCORE = 0.5
 
+--- Buildings Stuff ---
+NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.0
+NDefines.NBuildings.SUPPLY_ROUTE_RESOURCE_BONUS = 0.0
+
 ---Nation Stuff---
 NDefines.NCountry.BASE_RESEARCH_SLOTS = 3
-NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.006
+NDefines.NCountry.POPULATION_YEARLY_GROWTH_BASE = 0.015
 NDefines.NCountry.BASE_SURRENDER_LIMIT = 0.6
 NDefines.NCountry.BASE_MOBILIZATION_SPEED = 0.05
-NDefines.NCountry.BASE_COMMAND_POWER_GAIN = 0.5
-NDefines.NCountry.ATTACHE_XP_SHARE = 0.334
+NDefines.NCountry.BASE_COMMAND_POWER_GAIN = 0.25
+NDefines.NCountry.BASE_MAX_COMMAND_POWER = 100
+NDefines.NCountry.ATTACHE_XP_SHARE = 0.335
 NDefines.NCountry.SCORCHED_EARTH_STATE_COST = 15		--- Original 5, 
 
 ---Production Stuff---
@@ -60,13 +65,13 @@ NDefines.NRailwayGun.RAILWAY_GUN_RANGE = 10		--- Range in Pixels. Default is 30.
 NDefines.NAI.ESTIMATED_CONVOYS_PER_DIVISION = 1		---Base is 6. Pending Change.
 
 NDefines.NAI.ORG_UNIT_STRONG = 0.65
-NDefines.NAI.STR_UNIT_STRONG = 0.65
+NDefines.NAI.STR_UNIT_STRONG = 0.75
 
-NDefines.NAI.ORG_UNIT_WEAK = 0.1
-NDefines.NAI.STR_UNIT_WEAK = 0.15
+NDefines.NAI.ORG_UNIT_WEAK = 0.25
+NDefines.NAI.STR_UNIT_WEAK = 0.35
 
-NDefines.NAI.ORG_UNIT_NORMAL = 0.3
-NDefines.NAI.STR_UNIT_NORMAL = 0.35
+NDefines.NAI.ORG_UNIT_NORMAL = 0.45
+NDefines.NAI.STR_UNIT_NORMAL = 0.55
 
 NDefines.NAI.WANTED_UNITS_MANPOWER_DIVISOR = 5000
 
@@ -75,11 +80,13 @@ NDefines.NSupply.MAX_RAILWAY_LEVEL = 3		--- Base is 5. Pending Change.
 NDefines.NSupply.CAPITAL_SUPPLY_MILITARY_FACTORIES = 0.6
 NDefines.NSupply.CAPITAL_SUPPLY_CIVILIAN_FACTORIES = 0.6
 
-NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.55 		--- Base is 0.5, less provinces and more cluster VP equals less supply issues. Pending Change
+NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.4 		--- Base is 0.5, less provinces and more cluster VP equals less supply issues. Pending Change
 
-NDefines.NSupply.SUPPLY_FLOW_DROP_REDUCTION_AT_MAX_INFRA = 0.20		--- Base is 0.3
+NDefines.NSupply.SUPPLY_FLOW_DROP_REDUCTION_AT_MAX_INFRA = 0.25		--- Base is 0.3
 
-NDefines.NSupply.INFRA_TO_SUPPLY = 0.75
+NDefines.NSupply.NODE_FLOW_BONUS_PER_RAIL_LEVEL = 0.5
+
+NDefines.NSupply.INFRA_TO_SUPPLY = 0.8
 NDefines.NSupply.VP_TO_SUPPLY_BASE = 0.75
 NDefines.NSupply.SUPPLY_FROM_DAMAGED_INFRA = 0.2
 NDefines.NSupply.SUPPLY_DISRUPTION_DAILY_RECOVERY = 0.5
@@ -87,5 +94,5 @@ NDefines.NSupply.SUPPLY_DISRUPTION_DAILY_RECOVERY = 0.5
 NDefines.NSupply.SUPPLY_THRESHOLD_FOR_ARMY_ATTRITION = 0.25		--- Base is 0.35. Attrition is bad in the Metro yo,
 
 --La Resistance--
-NDefines.NOperatives.AGENCY_CREATION_DAYS = 9999			--- No more Espionage!
+NDefines.NOperatives.AGENCY_CREATION_DAYS = 99				--- No more Espionage!
 NDefines.NOperatives.AGENCY_CREATION_FACTORIES = 999		--- No more Espionage!
