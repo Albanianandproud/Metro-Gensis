3e1bf1de60a9cd860941a4cefdb3e901e8cf50c3
Albanianandproud <Bigdickautomobiles@gmail.com>
2022-08-08 22:05:19 +0100

    preliminary update

--- Metro/common/defines/01_defines.lua
+++ Metro/common/defines/01_defines.lua
@@ -7,6 +7,7 @@ NDefines.NGame.END_DATE = "2055.1.1.1"
 NDefines.NDiplomacy.TRUCE_BREAK_COST_PP = 100
 NDefines.NDiplomacy.NAP_EXPIRY_MONTHS = 24
 NDefines.NDiplomacy.NAP_UNBREAKABLE_MONTHS = 6
+NDefines.NDiplomacy.VOLUNTEERS_TRANSFER_SPEED = 2
 NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 5
 NDefines.NDiplomacy.TENSION_TIME_SCALE_START_DATE = "2028.1.1.12"
 NDefines.NDiplomacy.GUARANTEE_COST = 100			
@@ -33,8 +34,8 @@ NDefines.NCountry.BASE_COMMAND_POWER_GAIN = 0.25
 NDefines.NCountry.BASE_MAX_COMMAND_POWER = 100
 NDefines.NCountry.LOCAL_MANPOWER_ACCESSIBLE_NON_CORE_FACTOR = 0.1
 NDefines.NCountry.MAX_NON_CORE_MANPOWER_FACTOR = 0.75
-NDefines.NCountry.ATTACHE_XP_SHARE = 0.335
-NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.05		
+NDefines.NCountry.ATTACHE_XP_SHARE = 0.4
+NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.025		
 NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 5
 NDefines.NCountry.SCORCHED_EARTH_STATE_COST = 15		--- Original 5
 
@@ -56,14 +57,32 @@ NDefines.NMilitary.INFRA_ORG_IMPACT = 0.5
 NDefines.NMilitary.ENGAGEMENT_WIDTH_PER_WIDTH = 4.0
 NDefines.NMilitary.INFRASTRUCTURE_MOVEMENT_SPEED_IMPACT = -0.02
 NDefines.NMilitary.BASE_FORT_PENALTY = -0.05						--- Base is 0.15
-NDefines.NMilitary.TRAINING_ORG = 0.35								--- Base is 0.2
-NDefines.NMilitary.TRAINING_MIN_STRENGTH = 0.35						--- if strength is less than this, the unit will pause training until it's been reinforced
+NDefines.NMilitary.TRAINING_ORG = 0.45								--- Base is 0.2
+NDefines.NMilitary.TRAINING_MIN_STRENGTH = 0.4						--- if strength is less than this, the unit will pause training until it's been reinforced
 NDefines.NMilitary.TRAINING_MAX_DAILY_COUNTRY_EXP = 0.2				--- Maximum army XP gained per day from training
+NDefines.NMilitary.FIELD_EXPERIENCE_SCALE = 0.0005
+NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY = 0.7
 
 ---Commanders
 NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 20	--- Base is 24
 NDefines.NMilitary.DIVISION_SIZE_FOR_XP = 2				--- Base is 8
 NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 20		--- Base is 24
+
+--- Resistance
+
+NDefines.NResistance.INITIAL_STATE_RESISTANCE = 5
+NDefines.NResistance.RESISTANCE_TARGET_BASE = 45.0 
+NDefines.NResistance.RESISTANCE_TARGET_MODIFIER_IS_AT_PEACE = 0
+
+NDefines.NResistance.RESISTANCE_TARGET_MODIFIER_STATE_VP = {					-- resistance target modifier pairs for vp. first entry is total vp in state and second entry is amount of target modifier that applies for that threshold
+		5,   2.5, -- 0 - 5
+		10,   7.5, -- 5 - 20
+		25,  15.0, -- 20 - 50
+		50,  30.0, -- 50 - ...
+	}
+
+NDefines.NResistance.RESISTANCE_POP_LOW_CUTOFF = 5
+NDefines.NResistance.RESISTANCE_POP_VERY_LOW_CUTOFF = 1
 
 ---Division Designer
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 5			
@@ -102,22 +121,27 @@ NDefines.NAI.NUM_SILOS_PER_CIVILIAN_FACTORIES = 0.0001
 NDefines.NAI.NUM_SILOS_PER_MILITARY_FACTORIES = 0.0001
 NDefines.NAI.NUM_SILOS_PER_DOCKYARDS = 0.001
 ---Supply Stuff---
-NDefines.NSupply.MAX_RAILWAY_LEVEL = 3		--- Base is 5. Pending Change.
+NDefines.NSupply.MAX_RAILWAY_LEVEL = 1		--- Base is 5. Pending Change.
 NDefines.NSupply.CAPITAL_SUPPLY_MILITARY_FACTORIES = 0.75
 NDefines.NSupply.CAPITAL_SUPPLY_CIVILIAN_FACTORIES = 0.75
 
-NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.5		--- Base is 0.5, less provinces and more cluster VP equals less supply issues. Pending Change
+NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.25		--- Base is 0.5, less provinces and more cluster VP equals less supply issues. Pending Change
 
-NDefines.NSupply.SUPPLY_FLOW_DROP_REDUCTION_AT_MAX_INFRA = 0.25		--- Base is 0.3
+NDefines.NSupply.SUPPLY_FLOW_DROP_REDUCTION_AT_MAX_INFRA = 0.5	--- Base is 0.3
 
-NDefines.NSupply.NODE_FLOW_BONUS_PER_RAIL_LEVEL = 0.5
+NDefines.NSupply.NODE_FLOW_BONUS_PER_RAIL_LEVEL = 1
 
-NDefines.NSupply.INFRA_TO_SUPPLY = 0.75
-NDefines.NSupply.VP_TO_SUPPLY_BASE = 0.75
+NDefines.NSupply.INFRA_TO_SUPPLY = 1
+NDefines.NSupply.VP_TO_SUPPLY_BASE = 1
 NDefines.NSupply.SUPPLY_FROM_DAMAGED_INFRA = 0.25
-NDefines.NSupply.SUPPLY_DISRUPTION_DAILY_RECOVERY = 0.75
+NDefines.NSupply.SUPPLY_DISRUPTION_DAILY_RECOVERY = 0.65
 
-NDefines.NSupply.SUPPLY_THRESHOLD_FOR_ARMY_ATTRITION = 0.2		--- Base is 0.35. Attrition is bad in the Metro yo,
+NDefines.NSupply.SUPPLY_POINTS_PER_TRAIN = 3.0
+
+NDefines.NSupply.RAILWAY_BASE_FLOW = 15.0 		
+NDefines.NSupply.RAILWAY_FLOW_PER_LEVEL = 2.50
+
+NDefines.NSupply.SUPPLY_THRESHOLD_FOR_ARMY_ATTRITION = 0.25		--- Base is 0.35. Attrition is bad in the Metro yo,
 
 --La Resistance--
 NDefines.NOperatives.AGENCY_CREATION_DAYS = 99				--- No more Espionage!
